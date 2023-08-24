cf5f442b9b1a0fb3bf745a501d04915f8c32eb8a
Simon Ihmig <simon.ihmig@gmail.com>
2023-08-24 23:20:47 +0200

    Setup tailwind + toucan styles

---
+++ config/tailwind/tailwind.config.js
@@ -0,0 +1,18 @@
+'use strict';
+// located in <app root>/config/tailwind/
+
+const path = require('path');
+
+const appRoot = path.join(__dirname, '../../');
+const appEntry = path.join(appRoot, 'app');
+const relevantFilesGlob = '**/*.{html,js,ts,hbs,gjs,gts}';
+
+module.exports = {
+  content: [path.join(appEntry, relevantFilesGlob)],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  presets: [require('@crowdstrike/tailwind-toucan-base')],
+  safelist: ['theme-dark', 'theme-light'],
+};
